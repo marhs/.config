f19cbdce1e84e920113c1aa9022abb889e07dd79
Marco Herrero <mherrero@kooth.com>
2022-12-19 15:04:11 +0100

    Refactor and back to lua

---
+++ nvim/after/plugin/telescope.lua
@@ -0,0 +1,30 @@
+local telescope = require'telescope'
+local builtin = require('telescope.builtin')
+
+telescope.setup {
+  extensions = {
+    project = {
+      base_dirs = {
+        {path = '~/kooth/projects', max_depth = 2},
+      },
+      hidden_files = true, -- default: false
+      theme = "dropdown",
+      order_by = "recent",
+      sync_with_nvim_tree = true, -- default false
+    }
+  }
+}
+
+telescope.load_extension('project')
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+
+vim.api.nvim_set_keymap(
+        'n',
+        '<leader>cd',
+        ":lua require'telescope'.extensions.project.project{}<CR>",
+        {noremap = true, silent = true}
+)
