910de1baa4b4d148c5efb029c63f066b7fa62b9f
Marco Herrero <mherrero@kooth.com>
2023-03-21 12:29:48 +0100

    Highlight yank

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -1,6 +1,7 @@
 vim.opt.guicursor = ""
 
 vim.opt.nu = true
+vim.opt.relativenumber = false
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -11,7 +12,7 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
@@ -26,3 +27,9 @@ vim.opt.splitbelow = true
 vim.opt.splitright = true
 
 vim.opt.colorcolumn = "80"
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = "1000" })
+  end,
+})
