4aaf669b65f106b3b907ad0213dc0ba21eb0c238
Marco Herrero <mherrero@kooth.com>
2023-03-24 11:10:24 +0100

    Add formatting to LSP

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -4,16 +4,17 @@ lsp.preset('recommended')
 
 lsp.ensure_installed({
   'pyright',
-  'sumneko_lua',
+  --'sumneko_lua',
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
+  require("lsp-format").on_attach(client)
 
-  if client.name == "eslint" then
-      vim.cmd.LspStop('eslint')
-      return
-  end
+  --if client.name == "eslint" then
+      -- vim.cmd.LspStop('eslint')
+      -- return
+  --end
 
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
   vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
@@ -24,6 +25,7 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
   vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
   vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+  vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, opts)
   vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 end)
 
@@ -33,4 +35,3 @@ vim.diagnostic.config({
 
 lsp.nvim_workspace()
 lsp.setup()
-
