cf95bbbd2bc6a37298ff9af45509a3d10fa95312
Marco Herrero <mherrero@kooth.com>
2024-02-23 16:37:03 +0100

    Reorder again

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,8 +2,8 @@ if vim.g.vscode then
     -- VSCode extension
 else
     -- ordinary Neovim
-    require("config.lazy")
     require("config.remap")
+    require("config.lazy")
     require("config.set")
     require("config.colors")
     require("config.neovide")
